7b3c910efe9fac9498c01f29e6d4b13128c860b4
BenRamsden <benramsdensii@gmail.com>
2017-02-12 15:50:59 +0000

    Removed test implementation of SensorAudio in preperation for spectrogram (histogram) implementation using the android RecordAudio class

--- NavigationApp/app/src/main/java/ramsden/benjamin/navigationapp/SensorAudio.java
+++ NavigationApp/app/src/main/java/ramsden/benjamin/navigationapp/SensorAudio.java
@@ -1,69 +1,35 @@
 package ramsden.benjamin.navigationapp;
 
 import android.content.Context;
-import android.media.MediaRecorder;
 import android.support.annotation.Nullable;
 import android.util.Log;
-
-import java.io.IOException;
 
 /**
  * Created by ben on 09/02/2017.
  */
 
 public class SensorAudio {
-    private MediaRecorder mRecorder = null;
-    private String mFileName = null;
 
     public SensorAudio(Context context) {
         // Record to the external cache directory for visibility
-        mFileName = context.getExternalCacheDir().getAbsolutePath();
-        mFileName += "/sensoraudiotest.3gp";
+
     }
 
     public void start() {
         Log.d(Constants.SENSOR_MICROPHONE, "startScan");
-
-        try {
-            if (mRecorder == null) {
-                mRecorder = new MediaRecorder();
-                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                mRecorder.setOutputFile(mFileName);
-                mRecorder.prepare();
-                mRecorder.start();
-            }
-        } catch(IOException ex) {
-            Log.d(Constants.SENSOR_MICROPHONE, "IOException: " + ex);
-            mRecorder = null;
-        }
 
     }
 
     public void stop() {
         Log.d(Constants.SENSOR_MICROPHONE, "stop");
 
-        if (mRecorder != null) {
-            try {
-                mRecorder.stop();
-                mRecorder.release();
-                mRecorder = null;
-            } catch (RuntimeException ex) {
-                Log.d(Constants.SENSOR_MICROPHONE, "Failed to stop mRecorder with ex: " + ex);
-            }
-        }
     }
 
     @Nullable
     public Integer getAmplitude() {
         Log.d(Constants.SENSOR_MICROPHONE, "getAmplitude");
 
-        if (mRecorder != null) {
-            return  mRecorder.getMaxAmplitude();
-        }
-
-        return null;
+        return 0;
 
     }
 }
